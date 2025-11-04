4a8ed6833cf07c90af314bda54d4639bc545a32c
patylu001 <patricia.ortegon@gmail.com>
2025-11-03 18:32:13 -0600

    Implement database connection and query execution
    Updated main method to connect to MySQL database and execute a query.

--- src/main/java/com/fca/calidad/maven/App.java
+++ src/main/java/com/fca/calidad/maven/App.java
@@ -4,7 +4,25 @@ package com.fca.calidad.maven;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+   public static void main(String args[]) throws SQLException {
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String dbURL = "jdbc:mysql://localhost:3307/calidad";
+            System.out.println("jdbcurl=" + dbURL);
+            String strUserID = "root";
+            String strPassword = "123456";
+            con=DriverManager.getConnection(dbURL,strUserID,strPassword);
+            System.out.println("Connected to the database.");
+            Statement stmt=(Statement) con.createStatement();
+            System.out.println("Executing query");
+            ResultSet rs=((java.sql.Statement) stmt).executeQuery("SELECT * FROM usuarios");
+            while(rs.next())
+                System.out.println(rs.getInt("1"));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+        finally {
+            con.close();
+        }
+}
 }
